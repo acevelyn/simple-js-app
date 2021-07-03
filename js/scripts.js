7bed0c1283ab136f7ef3e9f95b0da97a2b393e57
acevelyn <86307435+acevelyn@users.noreply.github.com>
2021-07-03 11:00:49 -0400

    Added Pokemon List with their attributes

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-alert('Hello World');
-
-let favoriteFood = 'Fries';
-document.write(favoriteFood);
+// Lisiting each pokemon with a description of name, height and types for each
+let pokemonList = [
+{name: "Bulbasaur", height: 5, types: ['grass', 'poison']},
+{name: "Pikachu", height: 7, types:['field', 'fairy']  },
+{name: "Eevee", height: 9, types: ['field'] }
+];
