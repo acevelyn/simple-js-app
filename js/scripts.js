fa88120dff0cfd89f59131b6cdb3ab47855c8fed
acevelyn <86307435+acevelyn@users.noreply.github.com>
2021-07-09 09:33:25 -0400

    Listed each Pokemon and their height on page

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,8 @@ let pokemonList = [
 {name: "Pikachu", height: 7, types:['field', 'fairy']  },
 {name: "Eevee", height: 9, types: ['field'] }
 ];
+
+
+for (let i=0; i < pokemonList.length; i++){
+  document.write('<p>' + pokemonList[i].name + " " + '(height: ' + pokemonList[i].height + ')');
+}
