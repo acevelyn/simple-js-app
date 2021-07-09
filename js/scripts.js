333708f700621d6af1ed73c99aeb0153c77d1c55
acevelyn <86307435+acevelyn@users.noreply.github.com>
2021-07-09 09:40:32 -0400

    Updated/Created condition for Pokemon's height

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,18 @@
 // Lisiting each pokemon with a description of name, height and types for each
 let pokemonList = [
-{name: "Bulbasaur", height: 5, types: ['grass', 'poison']},
-{name: "Pikachu", height: 7, types:['field', 'fairy']  },
-{name: "Eevee", height: 9, types: ['field'] }
+{name: "Bulbasaur", height: 1.0, types: ['grass', 'poison']},
+{name: "Pikachu", height: 0.5, types:['field', 'fairy']  },
+{name: "Eevee", height: 0.8, types: ['field'] }
 ];
 
 
+
+
+// listing each pokemon name and height on Page
 for (let i=0; i < pokemonList.length; i++){
   document.write('<p>' + pokemonList[i].name + " " + '(height: ' + pokemonList[i].height + ')');
+// if a pokemon's height is > or = to 1.0 print message beside it
+  if(pokemonList[i].height >= 1.0){
+    document.write(' Wow that\'s big!');
+  }
 }
